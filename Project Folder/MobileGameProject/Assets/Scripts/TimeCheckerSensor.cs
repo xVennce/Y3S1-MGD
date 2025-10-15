2ce0d65d2f8e44109e972194304e34976d72cc48
xVennce <145119672+xVennce@users.noreply.github.com>
2025-10-15 16:10:45 +0100

    Made a script that does nothing atm

---
+++ Project Folder/MobileGameProject/Assets/Scripts/TimeCheckerSensor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TimeCheckerSensor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string CheckLiveTime()
+    {
+        return "";
+    }   
+}
