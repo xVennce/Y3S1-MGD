3e742edd5c86ec1f7473b03d213540c25f28b8fc
xVennce <145119672+xVennce@users.noreply.github.com>
2025-10-18 22:39:12 +0100

    Did a get location api call but not sure going to use since the dark sky api is gone

--- Project Folder/MobileGameProject/Assets/Scripts/ButtonAgent.cs
+++ Project Folder/MobileGameProject/Assets/Scripts/ButtonAgent.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 
-public class ButtonAgent : MonoBehaviour
-{
+public class ButtonAgent : MonoBehaviour {
+    [Header("Game Data")]
     [SerializeField] GameData gameData;
+    
+
+
     public void SaveData() {
         SaveLoadSystem.SavePlayer(gameData);
     }
     public void LoadData() {
         PlayerData data = SaveLoadSystem.LoadPlayer();
-        if (data != null)
-        {
+        if (data != null) {
             gameData.Money = data.Money;
             gameData.PlantGrowthStage = data.CurrentPlantGrowthStage;
             Debug.Log("Money loaded: " + data.Money);
             Debug.Log("Plant Growth Stage loaded: " + data.CurrentPlantGrowthStage);
         }
     }
+
+
 }
---
+++ Project Folder/MobileGameProject/Assets/Scripts/WeatherAPI/GetLocation.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Collections;
+
+public class GetLocation : MonoBehaviour {
+
+    private string DeviceIP;
+
+    public LocationData DeviceLocationInfo;
+    public float Latitude;
+    public float Longitude;
+
+    private void Start() {
+        StartCoroutine(GetDeviceIP());
+    }
+
+    /// <summary>
+    /// This coroutine fetches the device's public IP address using an external API.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator GetDeviceIP() {
+        using UnityWebRequest DeviceIPRequest = UnityWebRequest.Get("https://api.ipify.org?format=text");
+        yield return DeviceIPRequest.SendWebRequest();
+
+        if (DeviceIPRequest.result != UnityWebRequest.Result.Success) {
+            Debug.LogError("Error fetching IP: " + DeviceIPRequest.error);
+            yield break;
+        }
+
+        DeviceIP = DeviceIPRequest.downloadHandler.text;
+        Debug.Log("Device IP: " + DeviceIP);
+        StartCoroutine(GetDeviceCoordinates());
+    }
+    /// <summary>
+    /// This coroutine gets the device location data and assigns longitude and latitude to the corresponding one.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator GetDeviceCoordinates() {
+        using UnityWebRequest DeviceLocationRequest = UnityWebRequest.Get("http://ip-api.com/json/" + DeviceIP);
+        yield return DeviceLocationRequest.SendWebRequest();
+        
+        if (DeviceLocationRequest.result != UnityWebRequest.Result.Success) {
+            Debug.LogError("Error fetching location: " + DeviceLocationRequest.error);
+            yield break;
+        }
+
+        DeviceLocationInfo = JsonUtility.FromJson<LocationData>(DeviceLocationRequest.downloadHandler.text);
+        Longitude = DeviceLocationInfo.lon;
+        Latitude = DeviceLocationInfo.lat;
+    }
+
+    [Serializable]
+    public class LocationData {
+        public string status;
+        public string country;
+        public string countryCode;
+        public string region;
+        public string regionName;
+        public string city;
+        public string zip;
+        public float lat;
+        public float lon;
+        public string timezone;
+        public string isp;
+        public string org;
+        public string @as;
+        public string query;
+    }
+}
