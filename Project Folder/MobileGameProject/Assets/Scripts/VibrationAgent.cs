1d2ceb9c817dadf3afc5e452b67d79ebd717d3ac
xVennce <145119672+xVennce@users.noreply.github.com>
2025-10-18 01:33:29 +0100

    Added haptics into project (including code)

--- Project Folder/MobileGameProject/Assets/Scripts/VibrationAgent.cs
+++ Project Folder/MobileGameProject/Assets/Scripts/VibrationAgent.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
+using CandyCoded;
+using CandyCoded.HapticFeedback;
 
 public class VibrationAgent: MonoBehaviour {
     public static void VibrateDevice() {
         Debug.Log("VibrationPerformed");
         Handheld.Vibrate();
+    }
+    public static void LightHapticFeedback() {
+        Debug.Log("Light Haptic Feedback Performed");
+        HapticFeedback.LightFeedback();
+    }
+    public static void MediumHapticFeedback() {
+        Debug.Log("Medium Haptic Feedback Performed");
+        HapticFeedback.MediumFeedback();
+    }
+    public static void HeavyHapticFeedback() {
+        Debug.Log("Heavy Haptic Feedback Performed");
+        HapticFeedback.HeavyFeedback();
     }
 }
