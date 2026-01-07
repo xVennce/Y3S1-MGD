7b425a09d72c4a8ebc99b33d8d674586c6d38e00
xVennce <145119672+xVennce@users.noreply.github.com>
2026-01-07 21:25:26 +0000

    Deleted stuff for the great reset

--- Project Folder/MobileGameProject/Assets/Scripts/GameData.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class GameData : MonoBehaviour {
-    public float Money;
-    public float PlantGrowthStage;
-}   
--- Project Folder/MobileGameProject/Assets/Scripts/GameVolumeData.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class GameVolumeData : MonoBehaviour {
-    public float GlobalVolume, BackgroundVolume;
-}
