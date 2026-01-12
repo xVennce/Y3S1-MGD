f563f3615c1e3ef44d5fd2a69d341a1836c023f0
xVennce <145119672+xVennce@users.noreply.github.com>
2026-01-12 14:20:28 +0000

    Fixing shit

--- Project Folder/MobileGameProject/.utmp/RelWithDebInfo/564o4v1i/arm64-v8a/CMakeFiles/3.22.1-g37088a8-dirty/CMakeSystem.cmake
+++ Project Folder/MobileGameProject/.utmp/RelWithDebInfo/564o4v1i/arm64-v8a/CMakeFiles/3.22.1-g37088a8-dirty/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Windows-10.0.19045")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26200")
 set(CMAKE_HOST_SYSTEM_NAME "Windows")
-set(CMAKE_HOST_SYSTEM_VERSION "10.0.19045")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
 include("C:/Program Files/Unity/Hub/Editor/6000.0.58f2/Editor/Data/PlaybackEngines/AndroidPlayer/NDK/build/cmake/android.toolchain.cmake")
